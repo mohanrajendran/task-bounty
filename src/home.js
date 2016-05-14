ff07d791254db015499214e24c224c6717685ede
mohanrajendran <lynermohan@gmail.com>
2016-05-14 17:37:34 -0500

    Added submit

--- src/home.js
+++ src/home.js
@@ -5,15 +5,19 @@
         var home = this;
         
 		home.title = "TASK BOUNTY";	
+        
+        home.sub
 		
 		home.open_jobs = [];
 		
-		$http.get('/api/8380/task_bounty/openTasks')
+		$http.get('https://dev16788.service-now.com/api/8380/task_bounty/openTasks')
 			.then(function successCallback(response) {
 			home.open_jobs = response.data.result;
 		}, function errorCallback(response) {
 			console.log(response);
 		});
+        
+       
 	}; 
 	
 	app.controller("homeController",  ['$http', homepage]);
--- src/submit.js
+++ src/submit.js
@@ -1,23 +1,34 @@
 (function() {
     var app = angular.module("taskbounty", ["ngMaterial"]);
-    
-	var submitpage = function($http) {
+
+    var submitpage = function($http) {
         var submit = this;
-        
-		submit.title = "CREATE NEW BOUNTY";
-        
+
+        submit.title = "CREATE NEW BOUNTY";
+
         // form data
         submit.task = null;
         submit.latitute = null;
         submit.longitute = null;
         submit.bounty = null;
-        
+
         submit.create = function() {
-            if(submit.task && submit.latitute && submit.longitute && submit.bounty) {
-                
+            if (!submit.task || !submit.latitute || !submit.longitute || !submit.bounty) {
+                $http.post('https://dev16788.service-now.com/api/8380/task_bounty/submitTask', {
+                    lat: submit.latitude,
+                    lng: submit.longitute,
+                    summary: submit.task,
+                    totalBounty: submit.bounty,
+                    image: ""
+                })
+                    .then(function successCallback(response) {
+                        window.location.replace('home.html');
+                    }, function errorCallback(response) {
+                        console.log(response);
+                    });
             }
         };
-	}; 
-	
-	app.controller("submitController",  ['$http', submitpage]);
+    };
+
+    app.controller("submitController", ['$http', submitpage]);
 })();
